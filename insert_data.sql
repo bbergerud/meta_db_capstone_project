a077be16bbacbb847cfb50a0f947adacf1bbff5c
bbergerud <brandon.bergerud@gmail.com>
2024-05-29 21:03:11 -0400

    adding booking inserts

--- insert_data.sql
+++ insert_data.sql
@@ -14,6 +14,7 @@ INSERT INTO Staff (FullName, Role, Salary) VALUES
 ("Pegasus", "Waiter", 60000),
 ("Phil", "Chef", 100000);
 
+DELETE FROM Customers;
 INSERT INTO Customers (FullName, PhoneNumber, Email) VALUES
 ("Zeus", "123123123", "zeus@olympus.com"),
 ("Hades", "789789789", "hades@hades.com"),
@@ -23,3 +24,9 @@ INSERT INTO Orders (CustomerID, MenuID, Quantity, Price, StaffID, Date) VALUES
 (1, 7, 100, 1000, 1, CURDATE()),
 (2, 3, 3, 150, 2, CURDATE()),
 (3, 1, 1, 25, 1, CURDATE());
+
+INSERT INTO Bookings (Date, TableNumber, CustomerID) VALUES
+('2022-10-10', 5, 1),
+('2022-11-12', 3, 3),
+('2022-10-11', 2, 2),
+('2022-10-13', 2, 1);
